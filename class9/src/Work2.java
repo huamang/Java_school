96f8929d0a74ee170e0a4878cc7fb0a5324b4ad9
huamang <73064480+huamang@users.noreply.github.com>
2021-11-17 09:50:35 +0800

    计算器1

--- class9/src/Work2.java
+++ class9/src/Work2.java
@@ -1,56 +1,152 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Work2 {
-    public static void main(String[] argv){
-        JTextField jt1 = new JTextField();
-        JTextField jt2 = new JTextField();
-        JTextField jt3 = new JTextField();
-        JButton b = new JButton("=");
+public class Work2 extends JFrame implements ActionListener {
+    private JButton b;
+    private JTextField num1, num2, result, operator;
+    private JLabel err;
 
-        b.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String num1 = jt1.getText();
-                String num2 = jt3.getText();
-                String symbol = jt2.getText();
+    public Work2() {
+        super("计算器");
+        setSize(500,200);
+        setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        num1 = new JTextField("",8);
+        operator = new JTextField("",1);
+        num2 = new JTextField("",8);
+        result = new JTextField("",15);
+        err = new JLabel(" ");
+        b = new JButton("=");
+        result.setEditable(false);
+        add(num1);
+        add(operator);
+        add(num2);
+        add(result);
+        add(err);
+        add(b);
+
+        setVisible(true);
+
+        b.addActionListener(this);
+    }
 
 
+    // 获取操作数
+    public double getNum(JTextField jf,int num) throws NoneException,NumberFormantException{
+        double numb;
+        String s = jf.getText().trim();
+        if(s.length() == 0)
+            throw new NoneException("错误：第"+num+"个操作数为空");
+        try {
+            numb = Double.parseDouble(s);
+        }
+        catch(NumberFormatException e){
+            throw new NumberFormantException("错误：第"+num+"个操作数数据格式错误");
+        }
+        return numb;
+    }
+
+    // 获取操作符
+    public char getOp(JTextField jf) throws OpCharException{
+        String s = jf.getText().trim();
+        if(s.length() == 0) throw new OpCharException("错误：运算符为空");
+        if(s.length() > 1) throw new OpCharException("错误：运算符过多");
+        if("+-*/".indexOf(s)<0) throw new OpCharException("错误：无法识别的运算符");
+        return s.charAt(0);
+    }
+
+    // 计算
+    public double compute(double x,double y,char op) {
+        double r = 0;
+        if (y == 0 && op == '/')
+            throw new ArithmeticException("错误：除零错误");
+        switch (op) {
+            case '+':
+                r = x + y;
+                break;
+            case '-':
+                r = x - y;
+                break;
+            case '*':
+                r = x * y;
+                break;
+            case '/':
+                r = x / y;
+                break;
+        }
+        return r;
+    }
+
+    private void clickEq() {
+
+        double x,y,r;
+        char c;
+        //String r;
+
+        try {
+            x = getNum(num1,1);
+            y = getNum(num2,2);
+            c = getOp(operator);
+            r = compute(x,y,c);
+            result.setText(String.valueOf(r));//数字转字符串
+            err.setText(" ");
+        }
+        catch(Exception xx) {
+            result.setText("");
+            err.setText(xx.getMessage());
+        }
+        b.setText("clear");
+        num1.setEditable(false);
+        num2.setEditable(false);
+        operator.setEditable(false);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource() == b) {
+            if(b.getActionCommand().equals("=")) {
+                clickEq();
             }
-        });
-    }
-
-    public double figure(String num1, String num2, String symbol){
-        double n1 = Double.parseDouble(num1);
-        double n2 = Double.parseDouble(num2);
-        if(symbol == "+"){
-            return n1+n2;
-        }
-        else if(symbol == "-"){
-            return n1-n2;
-        }
-        else if(symbol == "*"){
-            return n1*n2;
-        }
-        else if(symbol == "/"){
-            return n1*n2;
-        }
-        else {
-            return 0;
+            else {
+                num1.setText("");
+                num2.setText("");
+                operator.setText("");
+                result.setText("");
+                err.setText("");
+                b.setText("=");
+                num1.setEditable(true);
+                num2.setEditable(true);
+                operator.setEditable(true);
+            }
         }
     }
 
-}
 
 
-class NullTextFieldException extends Exception {
+    public static void main(String[] argv) {
+        new Work2();
 
-    public NullTextFieldException() {
     }
 
-    public NullTextFieldException(String s) {
-        super(s);
+
+    class NoneException extends Exception {
+        public NoneException(String msg) {
+            super(msg);
+        }
     }
 
+    class OpCharException extends Exception {
+        public OpCharException(String msg) {
+            super(msg);
+        }
+    }
+
+    class NumberFormantException extends Exception {
+        public NumberFormantException(String msg) {
+            super(msg);
+        }
+    }
 }
