777ecb0ad19a4265ccdeb8b65add6c31def61c3a
huamang <73064480+huamang@users.noreply.github.com>
2021-11-23 23:56:49 +0800

    1

---
+++ class11/src/Book1.java
@@ -0,0 +1,15 @@
+public class Book1 {
+}
+class MyThread extends Thread{
+    private int n,max;
+    public MyThread(String name,int n1,int max1){
+        super(name);
+        n = n1;
+        max = max1;
+    }
+
+    public void run() {
+        
+
+    }
+}
